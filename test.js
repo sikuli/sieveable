e7b9bb4714e295737549a3e86b347f8377ad9f0a
Tom Yeh <doubleshow@gmail.com>
2015-02-16 15:19:52 -0700

    some changes

--- test.js
+++ test.js
@@ -2,9 +2,9 @@ var glob = require('glob')
 var fs = require('fs')
 
 var q1 = require('./lib/q1')
-var q2 = require('./lib/q2')
-var q3 = require('./lib/q3')
-var q4 = require('./lib/q4')
+// var q2 = require('./lib/q2')
+// var q3 = require('./lib/q3')
+// var q4 = require('./lib/q4')
 
 glob.glob('./data/ui-xml/*', function(err, files) {
 
