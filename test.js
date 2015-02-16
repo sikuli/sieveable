5fd9406a475d5860f4563993a54cf9a753046077
Tom Yeh <doubleshow@gmail.com>
2015-02-16 15:14:52 -0700

    some questions

---
+++ test.js
@@ -0,0 +1,23 @@
+var glob = require('glob')
+var fs = require('fs')
+
+var q1 = require('./lib/q1')
+var q2 = require('./lib/q2')
+var q3 = require('./lib/q3')
+var q4 = require('./lib/q4')
+
+glob.glob('./data/ui-xml/*', function(err, files) {
+
+    var apps = files.map(function(file) {
+
+        var app = {
+            xml: fs.readFileSync(file, 'utf8')
+        }
+
+        return app
+    })
+
+    var result = q1(apps)
+    console.log(result)
+
+})
