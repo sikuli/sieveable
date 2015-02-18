1cdc7924bc59ea127a2b0c672013b0aed59544f5
Tom Yeh <doubleshow@gmail.com>
2015-02-18 11:45:40 -0700

    q1

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ var fs = require('fs')
 
 var q1 = require('./lib/q1')
 // var q2 = require('./lib/q2')
-// var q3 = require('./lib/q3')
+var q3 = require('./lib/q3')
 // var q4 = require('./lib/q4')
 
 glob.glob('./data/ui-xml/*', function(err, files) {
@@ -17,6 +17,7 @@ glob.glob('./data/ui-xml/*', function(err, files) {
         return app
     })
 
+    // var result = q1(apps)
     var result = q1(apps)
     console.log(result)
 
