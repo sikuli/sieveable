27d786be1b6e8dc858f2f42c588f89e24d7d903a
Khalid Alharbi <Khalid3ahmed@gmail.com>
2015-02-23 08:48:36 -0700

    refactor q5

---
+++ examples/q5.example.js
@@ -0,0 +1,22 @@
+var glob = require('glob'),
+    fs = require('fs'),
+    q5 = require('../lib/q5');
+
+var n = process.argv.slice(2)[0] || 3
+
+console.time('q5')
+glob.glob('../data/ui-xml/*', function(err, files) {
+
+    var apps = files.map(function(file) {
+
+        var app = {
+            xml: fs.readFileSync(file, 'utf8')
+        }
+
+        return app
+    })
+    var result = q5(apps, n);
+    console.log(result)
+    console.timeEnd('q5')
+
+})
