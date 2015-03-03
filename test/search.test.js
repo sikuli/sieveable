d9f9d7b9d306977517179b9b05d6c93f7825a2e2
Tom Yeh <doubleshow@gmail.com>
2015-03-03 14:48:52 -0700

    more test cases for more queries

--- test/search.test.js
+++ test/search.test.js
@@ -9,7 +9,7 @@ describe('db', function() {
             name: 'LinearLayout'
         }
 
-        db.find(q, function(error, results){
+        db.find(q, function(error, results) {
 
             console.log(results.length)
             done()
@@ -30,7 +30,7 @@ describe('db', function() {
             }]
         }
 
-        db.find(q, function(error, results){
+        db.find(q, function(error, results) {
             console.log(results.length);
             done();
         });
@@ -45,13 +45,121 @@ describe('db', function() {
             children: [{
                 type: 'tag',
                 name: 'Button',
-                count: 3 
+                count: 3
             }]
         }
 
-        db.find(q, function(error, results){
-             console.log(results.length);
-             done();
+        db.find(q, function(error, results) {
+            console.log(results.length);
+            done();
+        });
+
+    })
+
+    it('find apps with a LinearLayout that contains exactly two buttons and two image views', function(done) {
+        this.timeout(10000);
+        var q = {
+            type: 'tag',
+            name: 'LinearLayout',
+            children: [{
+                type: 'tag',
+                name: 'Button',
+                count: 2
+            }, {
+                type: 'tag',
+                name: 'ImageView',
+                count: 2
+            }]
+        }
+
+        db.find(q, function(error, results) {
+            console.log(results.length);
+            done();
+        });
+
+    })
+
+    it('find apps with two siblings that are a button and an image view', function(done) {
+        this.timeout(10000);
+        var q = [{
+            type: 'tag',
+            name: 'Button',
+            count: 1
+        }, {
+            type: 'tag',
+            name: 'ImageView',
+            count: 1
+        }]
+
+        db.find(q, function(error, results) {
+            console.log(results.length);
+            done();
+        });
+
+    })
+
+    it('find apps with THREE siblings that are ONE button and TWO image views', function(done) {
+        this.timeout(10000);
+        var q = [{
+            type: 'tag',
+            name: 'Button',
+            count: 1
+        }, {
+            type: 'tag',
+            name: 'ImageView',
+            count: 2
+        }]
+
+        db.find(q, function(error, results) {
+            console.log(results.length);
+            done();
+        });
+
+    })
+
+    it('find apps with AT LEAST 20 buttons', function(done) {
+        this.timeout(10000);
+        var q = {
+            type: 'tag',
+            name: 'Button',
+            min: 20
+        }
+
+        db.find(q, function(error, results) {
+            console.log(results.length);
+            done();
+        });
+
+    })
+
+    it('find apps with AT MOST 5 buttons', function(done) {
+        this.timeout(10000);
+        var q = {
+            type: 'tag',
+            name: 'Button',
+            max: 5
+        }
+
+        db.find(q, function(error, results) {
+            console.log(results.length);
+            done();
+        });
+
+    })
+
+    it('find apps with a TextView with android:fadingEdge="horizontal"', function(done) {
+        this.timeout(10000);
+        var q = {
+            type: 'tag',
+            name: 'Button',
+            attrbs: {
+                'android:fadingEdge': 'horizontal'
+            }
+        }
+
+        db.find(q, function(error, results) {
+            console.log(results.length);
+            done();
         });
 
     })
