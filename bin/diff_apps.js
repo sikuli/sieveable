5f8a78c9be676d9a27e4088441054a9f664f8e5a
Khalid Alharbi <Khalid3ahmed@gmail.com>
2015-03-06 07:56:16 -0700

    refactor query by examples

--- bin/diff_apps.js
+++ bin/diff_apps.js
@@ -1,42 +1,42 @@
 #!/usr/bin/env node
 
 var program = require('commander'),
-    fs = require('fs')
-	// parser = require('../lib/parser.js');
+    fs = require('fs'),
+    parse = require('../lib/parse'),
+    db = require('../lib/db')
 
-var parse = require('../lib/parse')	
-var db    = require('../lib/db')	
-
-var ensureFileExists = function(file) {
-	if (!fs.existsSync(file) || !fs.lstatSync(file).isFile()) {
-		console.error('No such file, ' + file);
-		process.exit(-1);
-	}
+var ensureFileExists = function (file) {
+    if (!fs.existsSync(file) || !fs.lstatSync(file).isFile()) {
+        console.error('No such file, ' + file);
+        process.exit(-1);
+    }
 }
 program
-	.version("0.0.1")
-	.description("Android Apps Design Queries");
+    .version("0.0.1")
+    .description("Android Apps Design Queries");
 
 program
-	.command("find <xml_file> ")
-	.description("")
-	.action(function(xml_file) {
-		ensureFileExists(xml_file);
+    .command("find <xml_file> ")
+    .description("")
+    .action(function (xml_file) {
+        ensureFileExists(xml_file);
 
-		var text = fs.readFileSync(xml_file, 'utf8')
-		var q = parse(text)
-		db.find(q, function(err, result){
-			console.log(result)
-		})
-console.log(q);
-		// parser(xml_file, function(result, error) {
-		// 	console.log(result);
-		// });
-	});
+        var text = fs.readFileSync(xml_file, 'utf8')
+        var q = parse(text);
+        db.find(q, function (err, result) {
+            console.log(result);
+        })
+    });
+program
+    .command('*')
+    .action(function (env) {
+        console.error('Error: Unknown command.');
+        program.help();
+    });
 
 program.parse(process.argv);
 
-if (!program.args.length){
-	program.help();
+if (!program.args.length) {
+    program.help();
 }
 
