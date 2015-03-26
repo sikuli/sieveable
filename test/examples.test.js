763414a68a861d344345b925864cd605c9e05007
Khalid Alharbi <khalid3ahmed@gmail.com>
2015-03-26 02:24:07 -0600

    add test examples by sending HTTP requests

--- test/examples.test.js
+++ test/examples.test.js
@@ -1,29 +1,45 @@
-var expect = require('chai').expect,
-    fs = require('fs'),
-    path = require('path'),
+var fs = require('fs'),
+    request = require('supertest'),
     parse = require('../lib/parse.js'),
-    db = require('../lib/db.js')
+    app = require('../lib/server/server'),
+    should = require('should'),
+    chai = require("chai");
+
+chai.use(require('chai-things'));
+chai.should();
 
 //TODO: Validate results manually.
 describe('examples', function () {
+    var query_xml_q1_a = fs.readFileSync(__dirname +
+    '/../examples/queries/q1-a.xml', 'utf-8');
     this.timeout(0);
-
-    it('q1-a', function (done) {
-        var content = fs.readFileSync(__dirname + '/../examples/queries/q1-a.xml');
-        var q = parse(content);
-        db.find(q, function (err, results) {
-            expect(results).to.be.have.lengthOf(2);
-            done();
-        });
+    before(function () {
+        app.listen(3090, 'localhost', function (error) {
+        })
     })
 
-    it('q1-b', function (done) {
-        var content = fs.readFileSync(__dirname + '/../examples/queries/q1-b.xml');
-        var q = parse(content);
-        db.find(q, function (err, results) {
-            expect(results).to.be.have.lengthOf(60);
-            done();
-        });
-    })
+    it('q1-a it should search for the given example (' + query_xml_q1_a +
+        ' ) and returns packageName: "com.whatsapp", version: "48364" and "48450"',
+        function (done) {
+            var q = parse(query_xml_q1_a);
+            var expected_q1_a = [{
+                id: 56,
+                packageName: "com.whatsapp",
+                version: "48364"
+            },
+                {id: 57, packageName: "com.whatsapp", version: "48450"}]
+            request(app)
+                .get('/q/json')
+                .query({q: query_xml_q1_a})
+                .set('Accept', 'application/json')
+                .expect(200)
+                .end(function (err, res) {
+                    should.not.exist(err)
+                    should.exist(res.body)
+                    res.body.should.include.something.that.deep.equals(expected_q1_a[0])
+                    res.body.should.include.something.that.deep.equals(expected_q1_a[1])
+                    done()
+                });
+        })
 
 })
