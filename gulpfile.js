0fe0b7731ad60c252bd93b27819b7616a5147acc
Khalid Alharbi <khalid@engr2-12-225-dhcp.int.colorado.edu>
2015-04-09 15:52:02 -0600

    add extract archives task

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,29 @@
-var gulp = require('gulp'),
-    through2 = require('through2')
-
-
+var gulp = require('gulp');
+var through2 = require('through2');
+var runSequence = require('run-sequence');
+var tarbz2 = require('decompress-tarbz2');
+var vinylAssign = require('vinyl-assign');
 var buildIndex = require('./lib/findBy/tagname/searchIndex/build-stream')
 
-gulp.task('build:tagname', function() {
-    
-    gulp.src('data/ui-xml/*')
-        .pipe(buildIndex())    
+gulp.task('build:tagname', function () {
+
+    gulp.src('fixtures/ui-xml/*.xml')
+        .pipe(buildIndex())
         .pipe(gulp.dest('indexes/tagname'))
 })
+
+gulp.task('extract-archives', function () {
+    gulp.src('fixtures/listing/listing.tar.bz2')
+        .pipe(vinylAssign({extract: true}))
+        .pipe(tarbz2({strip: 1}))
+        .pipe(gulp.dest('fixtures/listing'))
+
+    gulp.src('fixtures/ui-xml/ui-xml.tar.bz2')
+        .pipe(vinylAssign({extract: true}))
+        .pipe(tarbz2({strip: 1}))
+        .pipe(gulp.dest('fixtures/ui-xml'))
+})
+
+gulp.task('default', function (callback) {
+    runSequence('extract-archives', 'build:tagname' , callback);
+});
