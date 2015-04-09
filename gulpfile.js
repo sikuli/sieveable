49314b69273ea20434a211139d026af77959b539
Khalid Alharbi <khalid@engr2-12-225-dhcp.int.colorado.edu>
2015-04-09 16:23:57 -0600

    add start mongodb task

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
 var gulp = require('gulp');
+var fs = require('fs');
 var through2 = require('through2');
 var runSequence = require('run-sequence');
 var tarbz2 = require('decompress-tarbz2');
 var vinylAssign = require('vinyl-assign');
+var exec = require('child_process').exec;
 var buildIndex = require('./lib/findBy/tagname/searchIndex/build-stream')
 
 gulp.task('build:tagname', function () {
@@ -24,6 +26,22 @@ gulp.task('extract-archives', function () {
         .pipe(gulp.dest('fixtures/ui-xml'))
 })
 
+gulp.task('start-db', function (callback) {
+    try {
+        fs.mkdirSync('fixtures/db');
+    } catch (error) {
+        if (error.code != 'EEXIST') callback(error);
+    }
+    exec('mongod --dbpath ./fixtures/db',
+        function (error, stdout, stderr) {
+            if (error) {
+                console.error(stderr);
+                return callback(err);
+            }
+            console.log(stdout);
+        });
+})
+
 gulp.task('default', function (callback) {
-    runSequence('extract-archives', 'build:tagname' , callback);
+    runSequence('extract-archives', 'build:tagname', 'start-db', callback);
 });
