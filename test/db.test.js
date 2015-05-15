74dbce35b450a6dc51449456ffcbc1d0d17e89e5
Khalid Alharbi <kalharbi@users.noreply.github.com>
2015-05-14 21:20:28 -0600

    use the new query syntax

--- test/db.test.js
+++ test/db.test.js
@@ -8,84 +8,51 @@ chai.should()
 
 var DB = require('../lib/db/index')
 
-describe('db', function() {
+describe('db', function () {
     this.timeout(20000)
 
-    it('can open', function() {
+    it('can open', function () {
 
         return DB.open()
-            .then(function(db) {
+            .then(function (db) {
                 db.should.have.property('env')
             })
     })
 
-    it('can find using a combined query', function() {
+    describe('findBy', function () {
 
-        return DB.open()
-            .then(function(db) {
-
-                var q = {
-                    listing: {
-                        category: 'game'
-                    },
-                    ui: {
-                        type: 'tag',
-                        name: 'LinearLayout',
-                        children: [{
-                            type: 'tag',
-                            name: 'ViewSwitcher',
-                            count: 1
-                        }]
-                    }
-                }
-
-                return db.find(q)
-
-            })
-            .then(function(results) {
-                // inspect(results)
-                
-            })
-    })
-
-    describe('findBy', function() {
-
-        describe('example', function() {
+        describe('example', function () {
 
             var env = {}
 
             var findBy = require('../lib/findBy/ui')
 
-            before(function() {
+            before(function () {
                 return findBy
                     .init(env)
             })
 
-            it('can init', function() {
+            it('can init', function () {
                 env.should.have.property('apps')
             })
 
-            describe('can find', function() {
+            describe('can find', function () {
 
-                it('LinearLayout and ViewSwitcher', function() {
+                it('TEST: LinearLayout and ViewSwitcher', function () {
 
-                    var q = {
-                        type: 'tag',
-                        name: 'LinearLayout',
-                        children: [{
-                            type: 'tag',
-                            name: 'ViewSwitcher',
-                            count: 1
-                        }]
-                    }
+                    var q = '<LinearLayout><ViewSwitcher/></LinearLayout>';
 
                     var options = {
-                        scope: _.range(1, 6)
+                        scope: ['com.sgiggle.production-1386724633',
+                            'com.sgiggle.production-68',
+                            'com.instagram.android-639564',
+                            'com.google.android.apps.plus-413076433',
+                            'com.cleanmaster.mguard-50511533']
                     }
 
                     findBy
                         .find(env, q, options)
-                        .then(function(results) {
+                        .then(function (results) {
                             inspect(results)
                         })
 
@@ -96,68 +63,61 @@ describe('db', function() {
         })
 
 
-        describe('tagname', function() {
+        describe('tagname', function () {
 
             var env = {}
 
             var findBy = require('../lib/findBy/tagname')
 
-            before(function() {
+            before(function () {
                 return findBy.init(env)
             })
 
-            it('can init', function() {
+            it('can init', function () {
                 env.indexes.should.have.property('tagname')
             })
 
-            describe('can find', function() {
+            describe('can find', function () {
 
-                it('LinearLayout and ViewSwitcher', function() {
+                it('LinearLayout and ViewSwitcher', function () {
 
-                    var q = {
-                        type: 'tag',
-                        name: 'LinearLayout',
-                        children: [{
-                            type: 'tag',
-                            name: 'ViewSwitcher',
-                            count: 1
-                        }]
+                    var q = '<LinearLayout><ViewSwitcher></ViewSwitcher></LinearLayout>';
+                    var options = {
+                        scope: ['com.sgiggle.production-1386724633',
+                            'com.sgiggle.production-68',
+                            'com.instagram.android-639564',
+                            'com.google.android.apps.plus-413076433',
+                            'com.cleanmaster.mguard-50511533']
                     }
 
                     findBy
-                        .find(env, q)
-                        .then(function(results) {
-                            // inspect(results)
+                        .find(env, q, options)
+                        .then(function (results) {
+                            inspect(results)
                         })
 
                 })
 
-                it('ViewSwitcher', function() {
+                it('ViewSwitcher', function () {
 
-                    var q = {
-                        type: 'tag',
-                        name: 'ViewSwitcher'
-                    }
+                    var q = '<ViewSwitcher/>';
 
                     findBy
                         .find(env, q)
-                        .then(function(results) {
-                            // inspect(results.length)
+                        .then(function (results) {
+                            inspect(results.length)
                         })
 
                 })
 
-                it('LinearLayout', function() {
+                it('LinearLayout', function () {
 
-                    var q = {
-                        type: 'tag',
-                        name: 'LinearLayout'
-                    }
+                    var q = '<LinearLayout/>';
 
                     findBy
                         .find(env, q)
-                        .then(function(results) {
-                            // inspect(results.length)
+                        .then(function (results) {
+                            inspect(results.length)
                         })
 
                 })
