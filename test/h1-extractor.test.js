edf622939e3a9771a466f7b018cb3032c370a727
Khalid Alharbi <khalid@vlan847-184-019.guest-wless.du.edu>
2015-06-09 09:47:45 -0600

    change index dir

--- test/h1-extractor.test.js
+++ test/h1-extractor.test.js
@@ -10,10 +10,10 @@ describe('test UI hierarchical index h1 extractor', function () {
     it('should extract parent-child tags', function (done) {
         var file = [path.resolve(__dirname + "/../fixtures/examples/index/me.pou.app-188.xml")];
         var expected = fs.readFileSync(__dirname + "/../fixtures/examples/index/me.pou.app-188-ui-h1.txt", "utf8");
-        var target = path.resolve(__dirname + "/../indexes/ui/h1-index/");
+        var target = path.resolve(__dirname + "/../indexes/ui/h1/");
         h1Extractor(file, target, function (err, res) {
             should.not.exist(err);
-            var targetFile = path.resolve(__dirname + "/../indexes/ui/h1-index/me.pou.app-188-ui-h1.txt");
+            var targetFile = path.resolve(__dirname + "/../indexes/ui/h1/me.pou.app-188-ui-h1.txt");
             var exists = fs.existsSync(targetFile);
             exists.should.equal(true);
             var result = fs.readFileSync(targetFile, "utf8");
