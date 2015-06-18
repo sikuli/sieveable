58b25ddb346eb7d35f624c28435045a45c3f3f36
Khalid Alharbi <kalharbi@users.noreply.github.com>
2015-06-18 04:59:37 -0600

    group tasks by type and reorder them

--- gulpfile.js
+++ gulpfile.js
@@ -3,18 +3,22 @@ var runSequence = require('run-sequence');
 var requireDir = require('require-dir');
 var tasks = requireDir('./tasks');
 
-gulp.task('load:db', function (callback) {
+gulp.task('mongo:insert', function (callback) {
     runSequence('mongo:insertListing', 'mongo:indexListing', 'mongo:close',
-        'redis:addKeys', callback)
+        callback);
 });
 
-gulp.task('solr:index', function (callback) {
+gulp.task('solr:insert', function (callback) {
     runSequence('extract:ui-tag', 'extract:ui-h1', 'extract:manifest',
         'solr:indexUITag', 'solr:indexH1', 'solr:indexManifest',
-        'solr:indexCode', 'solr:commit', callback)
+        'solr:indexCode', 'solr:commit', callback);
+});
+
+gulp.task('redis:insert', function (callback) {
+    runSequence('redis:addSolrKeys', 'redis:addAllKeys', callback);
 });
 
 gulp.task('default', function (callback) {
-    runSequence('solr:create', 'solr:addField', 'extract:archives', 'solr:index',
-        'load:db', callback);
+    runSequence('solr:create', 'solr:addField', 'extract:archives', 'solr:insert',
+        'mongo:insert', 'redis:insert', callback);
 });
