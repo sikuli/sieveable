d3f34d3c9325d2091a8d04a332dd9b9429edb606
Khalid Alharbi <kalharbi@users.noreply.github.com>
2015-06-28 17:52:22 -0600

    add manifest test case

--- test/manifest.test.js
+++ test/manifest.test.js
@@ -7,7 +7,9 @@ var eyes = require('eyes');
 var should = chai.should();
 
 var result_json_q1 = fs.readFileSync(__dirname +
-'/../fixtures/examples/manifest/q1.json', 'utf-8');
+    '/../fixtures/examples/manifest/q1.json', 'utf-8');
+var result_json_q2 = fs.readFileSync(__dirname +
+    '/../fixtures/examples/manifest/q2.json', 'utf-8');
 
 describe('Manifest Examples: Answers to multiple manifest by example questions.', function () {
     this.timeout(20000)
@@ -26,6 +28,36 @@ describe('Manifest Examples: Answers to multiple manifest by example questions.'
                     should.not.exist(err)
                     should.exist(res.body);
                     res.body.should.have.length(29);
+                    try {
+                        res.body.should.deep.include.members(expected)
+                    }
+                    catch (e) {
+                        console.log('Expected:')
+                        eyes.inspect(expected)
+                        console.log('Actual:')
+                        eyes.inspect(res.body)
+                        throw e
+                    }
+                    done()
+                });
+        })
+
+    it('Manifestoq2 It should search for apps that request 20 permissions\n' +
+        'and find 33 apps.',
+        function (done) {
+            var manifest_query = fs.readFileSync(__dirname +
+                '/../fixtures/examples/manifest/q2.xml', 'utf8');
+            var q = 'MATCH app\nWHERE\n' + manifest_query + '\nRETURN app';
+            var expected = JSON.parse(result_json_q2);
+            request(app)
+                .get('/q/json')
+                .query({queryText: q})
+                .set('Accept', 'application/json')
+                .expect(200)
+                .end(function (err, res) {
+                    should.not.exist(err)
+                    should.exist(res.body);
+                    res.body.should.have.length(33);
                     try {
                         res.body.should.deep.include.members(expected)
                     }
