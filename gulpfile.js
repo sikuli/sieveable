5da4f0bb7b0c4239ebef2d8e692b07c610cd361e
Khalid Alharbi <kalharbi@users.noreply.github.com>
2016-01-05 07:46:26 -0700

    remove readFullDB task

--- gulpfile.js
+++ gulpfile.js
@@ -19,7 +19,7 @@ gulp.task('redis:insert', (callback) => {
 
 gulp.task('leveldb:insert', (callback) => {
     runSequence('leveldb:create', 'leveldb:addListing', 'leveldb:addManifest',
-        'leveldb:addUI', 'leveldb:readFullDB', callback);
+        'leveldb:addUI', callback);
 });
 
 gulp.task('default', (callback) => {
