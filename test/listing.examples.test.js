09b3fc4daa86da5db3ad94f8fb37f671437ec409
Khalid Alharbi <kalharbi@users.noreply.github.com>
2016-01-11 11:16:37 -0700

    add logical operator queries for listing fields
    Add $gt, $gte, $lt, and $lte operators for two listing details fields: downloads and rating.
    Example query for apps with more than 4.0 star ratings: <rating>$gt:4.0</rating>.
    This closes #1

--- test/listing.examples.test.js
+++ test/listing.examples.test.js
@@ -9,7 +9,7 @@ const fs = require('fs'),
     should = chai.should();
 
 describe('Listing Details Examples: Answers to multiple listing details by example questions.', function () {
-    this.timeout(0)
+    this.timeout(0);
 
     var result_json_q1 = fs.readFileSync(__dirname +
         '/../fixtures/examples/listing/q1.json', 'utf-8');
@@ -48,7 +48,7 @@ describe('Listing Details Examples: Answers to multiple listing details by examp
                     }
                     done()
                 });
-        })
+        });
 
     it('q2: It should search for apps with downloads count between of 500,000,000' +
         ' and 1,000,000,000 and find 13 apps.',
@@ -78,7 +78,7 @@ describe('Listing Details Examples: Answers to multiple listing details by examp
                     }
                     done()
                 });
-        })
+        });
 
     it('q3: It should search for apps with the word PDF ' +
         'in their listing details using the full text index ' +
@@ -109,7 +109,7 @@ describe('Listing Details Examples: Answers to multiple listing details by examp
                     }
                     done()
                 });
-        })
+        });
 
     it('q4: It should search for apps by providing multiple listing details ' +
         'fields using the full text index along with absolute values search.',
@@ -141,6 +141,105 @@ describe('Listing Details Examples: Answers to multiple listing details by examp
                     }
                     done()
                 });
-        })
+        });
 
+    it('q5: It should search for apps that have 4.5 or more star ' +
+       'ratings and find 13 apps.', (done) => {
+        const listing_query = '<rating> $gte:4.5 </rating>',
+          q = 'MATCH app\nWHERE\n' + listing_query + '\n RETURN app';
+        request(app)
+            .get('/q/json')
+            .query({queryText: q})
+            .set('Accept', 'application/json')
+            .expect(200)
+            .end(function (err, res) {
+              should.not.exist(err);
+              should.exist(res.body);
+              res.body.should.have.length(13);
+              done();
+            });
+    });
+
+    it('q6: It should search for apps that have more than 4.5817795 star ' +
+       'ratings and find 6 apps.', (done) => {
+        const listing_query = '<rating> $gt:4.5817795 </rating>',
+          q = 'MATCH app\nWHERE\n' + listing_query + '\n RETURN app';
+        request(app)
+            .get('/q/json')
+            .query({queryText: q})
+            .set('Accept', 'application/json')
+            .expect(200)
+            .end(function (err, res) {
+              should.not.exist(err);
+              should.exist(res.body);
+              res.body.should.have.length(6);
+              done();
+            });
+    });
+
+    it('q7: It should search for apps that have less than or equal 3.8888636 star ' +
+       'ratings and find 8 apps.', (done) => {
+        const listing_query = '<rating> $lte:3.8888636 </rating>',
+          q = 'MATCH app\nWHERE\n' + listing_query + '\n RETURN app';
+        request(app)
+            .get('/q/json')
+            .query({queryText: q})
+            .set('Accept', 'application/json')
+            .expect(200)
+            .end(function (err, res) {
+              should.not.exist(err);
+              should.exist(res.body);
+              res.body.should.have.length(8);
+              done();
+            });
+    });
+
+    it('q8: It should search for apps that have less than 3.8888636 star ' +
+       'ratings and find 7 apps.', (done) => {
+        const listing_query = '<rating> $lt:3.8888636 </rating>',
+          q = 'MATCH app\nWHERE\n' + listing_query + '\n RETURN app';
+        request(app)
+            .get('/q/json')
+            .query({queryText: q})
+            .set('Accept', 'application/json')
+            .expect(200)
+            .end(function (err, res) {
+              should.not.exist(err);
+              should.exist(res.body);
+              res.body.should.have.length(7);
+              done();
+            });
+    });
+
+    it('q9: test an invalid listing query. It should return no apps and log an error message.', (done) => {
+        const listing_query = '<rating> </rating>',
+          q = 'MATCH app\nWHERE\n' + listing_query + '\n RETURN app';
+        request(app)
+            .get('/q/json')
+            .query({queryText: q})
+            .set('Accept', 'application/json')
+            .expect(200)
+            .end(function (err, res) {
+              should.not.exist(err);
+              should.exist(res.body);
+              res.body.should.have.length(0);
+              done();
+            });
+    });
+
+    it('q10: It should search for apps that have a minimum download count of 1,000,000,000 downloads and find one app.', (done) => {
+        const listing_query = '<downloads>1000000000</downloads>',
+          q = 'MATCH app\nWHERE\n' + listing_query + '\n RETURN app';
+        request(app)
+            .get('/q/json')
+            .query({queryText: q})
+            .set('Accept', 'application/json')
+            .expect(200)
+            .end(function (err, res) {
+              should.not.exist(err);
+              should.exist(res.body);
+              res.body.should.have.length(1);
+              done();
+            });
+    });
 })
