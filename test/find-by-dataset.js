a21ec21cb8f6b09c174676b8ceefab84967b4d0c
Khalid Alharbi <kalharbi@users.noreply.github.com>
2016-02-01 12:18:41 -0700

    fix computing the intersection of ids

--- test/find-by-dataset.js
+++ test/find-by-dataset.js
@@ -4,27 +4,38 @@ const chai = require('chai'),
     findByDataset = require('../lib/findBy/dataset');
 
 describe('test find by dataset.', function() {
-  it('should compute the intersection of ui, listing, code and manifest ids.', (done) => {
+  const env = {};
+
+  before((done) => {
+    // Initialize env.ids
+    env.ids = {};
+    env.ids.code = ['com.facebook.orca-936981', 'com.android.chrome-1547059', 'com.cleanmaster.mguard-40101776'];
+    env.ids.listing = ['com.adobe.reader-77969', 'com.facebook.katana-666397', 'com.android.chrome-1547059'];
+    env.ids.ui = ['com.facebook.katana-666397', 'com.android.chrome-1547059', 'com.outfit7.talkingtom2free-83'];
+    env.ids.manifest = ['com.android.chrome-1547059', 'com.opera.mini.android-25'];
+    done();
+  });
+
+  it('should compute the intersection of ids for ui, listing, code and manifest ids.', (done) => {
         this.timeout(0);
-        const env = {},
-            query = {
-                      match: [ 'app' ],
-                      listing: '<desc>ch*t SMS</desc><cat>(*)</cat>',
-                      ui: '<Button android:text="(*)"/>',
-                      manifest: '<uses-permission android:name="(android.permission.READ_*)"/>',
-                      code: '<code class="android.hardware.Camera" method="startPreview" />',
-                      return: [ 'app'],
-                      limit: 100,
-                      mode: 'strict'
-                    };
+        const query = {
+                  match: [ 'app' ],
+                  listing: '<desc>ch*t SMS</desc><cat>(*)</cat>',
+                  ui: '<Button android:text="(*)"/>',
+                  manifest: '<uses-permission android:name="(android.permission.READ_*)"/>',
+                  code: '<code class="android.hardware.Camera" method="startPreview" />',
+                  return: [ 'app'],
+                  limit: 100,
+                  mode: 'strict'
+              };
         return findByDataset.find(env, query)
-            .then((results) => {
-                results.should.have.length(55);
-                done();
-            }).catch((e) => {
-                console.error(e.message);
-                should.not.exist(e);
-                done(e);
-            });
+          .then((results) => {
+            results.should.have.length(1);
+            done();
+          }).catch((e) => {
+            console.error(e.message);
+            should.not.exist(e);
+            done(e);
+          });
   });
 });
