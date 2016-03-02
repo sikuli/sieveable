4cb167e1ac2eaa9e0729e0acab67f3079095283d
Khalid Alharbi <kalharbi@users.noreply.github.com>
2016-03-02 11:24:27 -0700

    limit linting to a set of dirs

--- tasks/lint.js
+++ tasks/lint.js
@@ -2,8 +2,7 @@ const gulp = require('gulp'),
   eslint = require('gulp-eslint');
 
 gulp.task('lint', () => {
-  // Files that are defined in .eslintignore are ignored by default.
-  return gulp.src(['**/*.js'])
+  return gulp.src(['lib/**/*.js', 'bin/**/*.js', 'tasks/**/*.js'])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
