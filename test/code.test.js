3da381b7016361dd3aadda593bd0959e1a89060b
Khalid Alharbi <kalharbi@users.noreply.github.com>
2016-03-05 19:38:52 -0700

    refactor

--- test/code.test.js
+++ test/code.test.js
@@ -1,81 +1,79 @@
-var fs = require('fs');
-var _ = require('lodash');
-var request = require('supertest');
-var parse = require('../lib/parse.js');
-var app = require('../lib/server/server');
-var chai = require("chai");
-var should = chai.should();
-var eyes = require('eyes');
+/* eslint-env node, mocha */
+/* eslint no-console: 0, max-statements:[2,20] */
+
+'use strict';
+const fs = require('fs'),
+  _ = require('lodash'),
+  request = require('supertest'),
+  path = require('path'),
+  app = require('../lib/server/server'),
+  chai = require('chai'),
+  should = chai.should(),
+  eyes = require('eyes');
+
+function testAPI(q, expected, deepMatch, callback) {
+  request(app)
+  .get('/q')
+  .query({ queryText: q })
+  .set('Accept', 'application/json')
+  .expect(200)
+  .end((err, res) => {
+    should.not.exist(err);
+    should.exist(res.body);
+    res.body.should.be.an('array', 'Response body is not an array');
+    res.body.should.have.length(expected.length);
+    let apps = {};
+    try {
+      if (deepMatch) {
+        apps = _.map(res.body, 'app');
+        apps.should.deep.include.members(expected);
+      }
+    }
+    catch (e) {
+      console.log('Expected:');
+      eyes.inspect(expected);
+      console.log('Actual:');
+      eyes.inspect(apps);
+      throw e;
+    }
+    callback();
+  });
+}
+
+function getFileContent(fileName) {
+  const fixturesDir = path.join(__dirname, '..', 'fixtures', 'examples', 'code');
+  return fs.readFileSync(path.join(fixturesDir, fileName), 'utf-8');
+}
 
 describe('Code Examples: Answers to multiple code by example questions.', function () {
-    this.timeout(0);
+  // The entire test suite may timeout in a minute
+  this.timeout(60000);
 
-    var result_json_q1 = fs.readFileSync(__dirname +
-        '/../fixtures/examples/code/q1.json', 'utf-8');
-    var result_json_q2 = fs.readFileSync(__dirname +
-        '/../fixtures/examples/code/q2.json', 'utf-8');
+  const resultQ1 = getFileContent('q1.json'),
+    resultQ2 = getFileContent('q2.json');
 
-    it('q1: It should search for apps that use the API call ' +
-        'android.hardware.CAMERA.takePicture\n' +
-        'and find 7 apps.', function (done) {
-        var code_query =
-            '<code class="android.hardware.Camera" method ="takePicture" />\n' +
-            '<code class="android.hardware.Camera" method ="startPreview" />'
-        var q = 'MATCH app\nWHERE\n' + code_query + '\nRETURN app';
-        var expected = JSON.parse(result_json_q1);
-        request(app)
-            .get('/q')
-            .query({queryText: q})
-            .set('Accept', 'application/json')
-            .expect(200)
-            .end(function (err, res) {
-                should.not.exist(err);
-                should.exist(res.body);
-                try {
-                    res.body.should.have.length(7);
-                    var apps = _.map(res.body, 'app');
-                    apps.should.deep.include.members(expected);
-                }
-                catch (e) {
-                    console.log('Expected:')
-                    eyes.inspect(expected)
-                    console.log('Actual:')
-                    eyes.inspect(res.body)
-                    throw e
-                }
-                done()
-            });
-    })
+  it('q1: It should search for apps that use the API calls: ' +
+    'android.hardware.CAMERA.takePicture and android.hardware.CAMERA.startPreview\n' +
+    'The result should be equal to 7 apps.', (done) => {
+    const codeQuery =
+          '<code class="android.hardware.Camera" method ="takePicture" />\n' +
+          '<code class="android.hardware.Camera" method ="startPreview" />',
+      q = `MATCH app\nWHERE\n${codeQuery}\nRETURN app`,
+      expected = JSON.parse(resultQ1);
+    testAPI(q, expected, true, () => {
+      done();
+    });
+  });
 
-    it('q2: It should search for apps that use the API call ' +
-        'com.google.android.gms.location.LocationListener.takePicture\n' +
-        'and find 7 apps.', function (done) {
-        var code_query =
-            '<code class="com.google.android.gms.location.LocationListener" method ="onLocationChanged" />\n';
-        var q = 'MATCH app\nWHERE\n' + code_query + '\nRETURN app';
-        var expected = JSON.parse(result_json_q2);
-        request(app)
-            .get('/q')
-            .query({queryText: q})
-            .set('Accept', 'application/json')
-            .expect(200)
-            .end(function (err, res) {
-                should.not.exist(err);
-                should.exist(res.body);
-                try {
-                    res.body.should.have.length(7);
-                    var apps = _.map(res.body, 'app');
-                    apps.should.deep.include.members(expected);
-                }
-                catch (e) {
-                    console.log('Expected:')
-                    eyes.inspect(expected)
-                    console.log('Actual:')
-                    eyes.inspect(res.body)
-                    throw e
-                }
-                done();
-            });
-    })
-
-})
+  it('q2: It should search for apps that use the API call ' +
+    'com.google.android.gms.location.LocationListener.takePicture\n' +
+    'and find 7 apps.', (done) => {
+    const codeQuery = '<code class="com.google.android.gms.location.LocationListener"' +
+      ' method ="onLocationChanged" />\n',
+      q = `MATCH app\nWHERE\n${codeQuery}\nRETURN app`,
+      expected = JSON.parse(resultQ2);
+    testAPI(q, expected, true, () => {
+      done();
+    });
+  });
+});
