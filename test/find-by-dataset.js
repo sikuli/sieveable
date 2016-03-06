5833ed837c66457c90f8e2873a2d4f5b8f1d3d69
Khalid Alharbi <kalharbi@users.noreply.github.com>
2016-03-05 22:29:13 -0700

    refactor

--- test/find-by-dataset.js
+++ test/find-by-dataset.js
@@ -1,41 +1,46 @@
+/* eslint-env node, mocha */
+/* eslint no-console: 0, max-statements:[2,20] */
 'use strict';
 const chai = require('chai'),
-    should = chai.should(),
-    findByDataset = require('../lib/findBy/dataset');
+  should = chai.should(),
+  findByDataset = require('../lib/findBy/dataset');
 
-describe('test find by dataset.', function() {
+describe('test find by dataset.', function () {
+  this.timeout(10000);
   const env = {};
 
   before((done) => {
     // Initialize env.ids
     env.ids = {};
-    env.ids.code = ['com.facebook.orca-936981', 'com.android.chrome-1547059', 'com.cleanmaster.mguard-40101776'];
-    env.ids.listing = ['com.adobe.reader-77969', 'com.facebook.katana-666397', 'com.android.chrome-1547059'];
-    env.ids.ui = ['com.facebook.katana-666397', 'com.android.chrome-1547059', 'com.outfit7.talkingtom2free-83'];
+    env.ids.code = ['com.facebook.orca-936981', 'com.android.chrome-1547059',
+      'com.cleanmaster.mguard-40101776'];
+    env.ids.listing = ['com.adobe.reader-77969', 'com.facebook.katana-666397',
+      'com.android.chrome-1547059'];
+    env.ids.ui = ['com.facebook.katana-666397', 'com.android.chrome-1547059',
+      'com.outfit7.talkingtom2free-83'];
     env.ids.manifest = ['com.android.chrome-1547059', 'com.opera.mini.android-25'];
     done();
   });
 
   it('should compute the intersection of ids for ui, listing, code and manifest ids.', (done) => {
-        this.timeout(0);
-        const query = {
-                  match: [ 'app' ],
-                  listing: '<desc>ch*t SMS</desc><cat>(*)</cat>',
-                  ui: '<Button android:text="(*)"/>',
-                  manifest: '<uses-permission android:name="(android.permission.READ_*)"/>',
-                  code: '<code class="android.hardware.Camera" method="startPreview" />',
-                  return: [ 'app'],
-                  limit: 100,
-                  mode: 'strict'
-              };
-        return findByDataset.find(env, query)
-          .then((results) => {
-            results.should.have.length(1);
-            done();
-          }).catch((e) => {
-            console.error(e.message);
-            should.not.exist(e);
-            done(e);
-          });
+    const query = {
+      match: ['app'],
+      listing: '<desc>ch*t SMS</desc><cat>(*)</cat>',
+      ui: '<Button android:text="(*)"/>',
+      manifest: '<uses-permission android:name="(android.permission.READ_*)"/>',
+      code: '<code class="android.hardware.Camera" method="startPreview" />',
+      return: ['app'],
+      limit: 100,
+      mode: 'strict'
+    };
+    return findByDataset.find(env, query)
+      .then((results) => {
+        results.should.have.length(1);
+        done();
+      }).catch((e) => {
+        console.error(e.message);
+        should.not.exist(e);
+        done(e);
+      });
   });
 });
