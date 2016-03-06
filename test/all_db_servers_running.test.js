905fd2b977bc7b3f1489d8a32bb40b32ad4db4ad
Khalid Alharbi <kalharbi@users.noreply.github.com>
2016-03-05 21:15:25 -0700

    refactor

--- test/all_db_servers_running.test.js
+++ test/all_db_servers_running.test.js
@@ -1,3 +1,7 @@
+/* eslint-env node, mocha */
+/* eslint no-console: 0, max-statements:[2,20] */
+'use strict';
+
 const Promise = require('bluebird'),
   config = require('config'),
   redis = require('redis'),
@@ -7,49 +11,47 @@ const Promise = require('bluebird'),
   should = chai.should();
 
 describe('Test that all external db servers are running using their ' +
-    'respected configuration options.', function () {
+  'respected configuration options.', function () {
+  this.timeout(10000);
+  it('It should ensure that Solr is running all the collections defined ' +
+      'in the config file exist.', (done) => {
+    Promise.all([
+      solrAdmin.exists(config.get('dbConfig.solr.listingCollection')),
+      solrAdmin.exists(config.get('dbConfig.solr.codeCollection')),
+      solrAdmin.exists(config.get('dbConfig.solr.uiSuffixCollection')),
+      solrAdmin.exists(config.get('dbConfig.solr.uiTagCollection')),
+      solrAdmin.exists(config.get('dbConfig.solr.manifestCollection'))])
+      .then(() => {
+        done();
+      }).catch((e) => {
+        console.error('ERROR: ', e);
+        should.not.exist(e);
+      });
+  });
 
-    it('It should ensure that Solr is running all the collections defined ' +
-        'in the config file exist.', function (done) {
-        Promise.all([
-            solrAdmin.exists(config.get('dbConfig.solr.listingCollection')),
-            solrAdmin.exists(config.get('dbConfig.solr.codeCollection')),
-            solrAdmin.exists(config.get('dbConfig.solr.uiSuffixCollection')),
-            solrAdmin.exists(config.get('dbConfig.solr.uiTagCollection')),
-            solrAdmin.exists(config.get('dbConfig.solr.manifestCollection'))])
-            .then(function () {
-                done();
-            })
-            .catch(function (e) {
-                console.error('ERROR: ' + e.message);
-                throw e;
-            })
-    })
-
-    it('It should ensure that Redis is running at localhost:6379 ' +
-        'and has all the keys defined in the config file.', function (done) {
-        var dbConfig = config.get('dbConfig.redis');
-        var datasetConfig = config.get('dataset');
-        var keys = [ datasetConfig.listingKeyName,
-            datasetConfig.uiKeyName,
-            datasetConfig.manifestKeyName,
-            datasetConfig.codeKeyName];
-        var client = redis.createClient();
-        client.on('ready', function () {
-            _.forEach(keys, function (key, idx) {
-                client.EXISTS(key, function (err, res) {
-                    res.should.equal(1);
-                    if (idx == keys.length - 1) {
-                        client.quit();
-                        done();
-                    }
-                });
-            })
-        });
-        client.on('error', function (error) {
+  it('It should ensure that Redis is running at localhost:6379 ' +
+    'and has all the keys defined in the config file.', (done) => {
+    const redisConfig = config.get('dbConfig.redis'),
+      datasetConfig = config.get('dataset'),
+      keys = [datasetConfig.listingKeyName,
+        datasetConfig.uiKeyName,
+        datasetConfig.manifestKeyName,
+        datasetConfig.codeKeyName],
+      client = redis.createClient({ host: redisConfig.host, port: redisConfig.port });
+    client.on('ready', () => {
+      _.forEach(keys, (key, idx) => {
+        client.exists(key, (err, res) => {
+          res.should.equal(1);
+          if (idx === keys.length - 1) {
             client.quit();
-            throw new Error('Redis client error ' + client.host + ':' +
-                client.port + ' - ' + error);
+            done();
+          }
         });
+      });
     });
-})
+    client.on('error', (error) => {
+      client.quit();
+      throw new Error(`Redis client error ${error.message}`);
+    });
+  });
+});
