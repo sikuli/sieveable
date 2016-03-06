7359916c58f3bb7d5fff3406e82c20b075dc406b
Khalid Alharbi <kalharbi@users.noreply.github.com>
2016-03-05 20:39:38 -0700

    refactor

--- test/api.test.js
+++ test/api.test.js
@@ -1,49 +1,53 @@
+/* eslint-env node, mocha */
+/* eslint no-console: 0, max-statements:[2,20] */
 'use strict';
 const request = require('supertest'),
   server = require('../lib/server/server'),
   chai = require('chai'),
-  _ = require('lodash'),
-  should = chai.should();
+  should = chai.should(),
+  _ = require('lodash');
 
-describe('Test API client errors.', function() {
+function testAPI(q, expectedCode, callback) {
+  request(server)
+  .get('/q')
+  .query(q)
+  .set('Accept', 'application/json')
+  .expect(expectedCode)
+  .end((err, res) => {
+    should.not.exist(err);
+    res.statusCode.should.equal(expectedCode);
+    res.body.should.be.an('object');
+    callback();
+  });
+}
+
+describe('Test API client errors.', function () {
   this.timeout(1000);
 
-  it('It should return 422 when submitting a query without the required field queryText', function(done) {
-    request(server)
-        .get('/q')
-        .query({})
-        .set('Accept', 'application/json')
-        .expect(422)
-        .end((err, res) => {
-          res.statusCode.should.equal(422);
-          res.body.should.be.an('object');
-          done();
-        });
+  it('It should return 422 when submitting a query without the ' +
+  'required field queryText', (done) => {
+    testAPI({}, 422, () => {
+      done();
+    });
   });
 
-  it('It should return 400 when submitting an invalid queryText', function(done) {
-    request(server)
-        .get('/q')
-        .query({ queryText: 'FOO BAR' })
-        .set('Accept', 'application/json')
-        .expect(400)
-        .end((err, res) => {
-          res.statusCode.should.equal(400);
-          res.body.should.be.an('object');
-          done();
-        });
+  it('It should return 400 when submitting an invalid queryText', (done) => {
+    testAPI({ queryText: 'FOO BAR' }, 400, () => {
+      done();
+    });
   });
 
-  it('It should return 404 for submitted queries', function(done) {
+  it('It should return 404 for submitted queries', (done) => {
     request(server)
-        .get('/foo')
-        .query({ foo: 'bar' })
-        .set('Accept', 'application/json')
-        .expect(404)
-        .end((err, res) => {
-          res.statusCode.should.equal(404);
-          _.isEmpty(res.body).should.equal(true);
-          done();
-        });
+    .get('/foo')
+    .query({ foo: 'bar' })
+    .set('Accept', 'application/json')
+    .expect(404)
+    .end((err, res) => {
+      res.statusCode.should.equal(404);
+      res.body.should.be.an('object');
+      _.isEmpty(res.body).should.equal(true);
+      done();
+    });
   });
 });
