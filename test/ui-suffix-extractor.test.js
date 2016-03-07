56c7a225915bba79792acb4e9f819341f54218b8
Khalid Alharbi <kalharbi@users.noreply.github.com>
2016-03-06 17:24:58 -0700

    fix invalid path name

--- test/ui-suffix-extractor.test.js
+++ test/ui-suffix-extractor.test.js
@@ -10,13 +10,14 @@ const chai = require('chai'),
 describe('test UI hierarchical index h1 extractor', function () {
   this.timeout(20000);
   it('should extract parent-child tags', (done) => {
-    const file = [path.resolve(__dirname, '../fixtures/examples/index/me.pou.app-188.xml')],
+    const file = [path.resolve(__dirname, '..', 'fixtures', 'examples',
+                  'index', 'me.pou.app-188.xml')],
       expected = fs.readFileSync(path.resolve(__dirname,
-           '/../fixtures/examples/index/me.pou.app-188-ui-suffix.txt'), 'utf8'),
-      target = path.resolve(__dirname, '../indexes/ui/suffix/');
+           '..', 'fixtures', 'examples', 'index', 'me.pou.app-188-ui-suffix.txt'), 'utf8'),
+      target = path.resolve(__dirname, '..', 'indexes', 'ui', 'suffix');
     suffixExtractor(file, target).then(() => {
-      const targetFile = path.resolve(__dirname,
-                           '../indexes/ui/suffix/me.pou.app-188-ui-suffix.txt'),
+      const targetFile = path.resolve(__dirname, '..', 'indexes', 'ui',
+                                      'suffix', 'me.pou.app-188-ui-suffix.txt'),
         result = fs.readFileSync(targetFile, 'utf8');
       result.should.equal(expected);
       done();
