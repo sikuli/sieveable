c7a02b0634e1a9dac895059cab4c23430981d0b0
Khalid Alharbi <kalharbi@users.noreply.github.com>
2016-03-06 17:27:29 -0700

    lint test files

--- tasks/lint.js
+++ tasks/lint.js
@@ -2,7 +2,7 @@ const gulp = require('gulp'),
   eslint = require('gulp-eslint');
 
 gulp.task('lint', () => {
-  return gulp.src(['lib/**/*.js', 'bin/**/*.js', 'tasks/**/*.js'])
+  return gulp.src(['lib/**/*.js', 'bin/**/*.js', 'tasks/**/*.js', 'test/**/*.js'])
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError());
