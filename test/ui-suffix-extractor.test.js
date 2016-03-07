89803b80cb2d97cee5c415d56a11cacfbf944913
Khalid Alharbi <kalharbi@users.noreply.github.com>
2016-03-06 17:19:42 -0700

    refactor

--- test/ui-suffix-extractor.test.js
+++ test/ui-suffix-extractor.test.js
@@ -1,27 +1,29 @@
+/* eslint-env node, mocha */
+/* eslint no-console: 0 */
 'use strict';
 const chai = require('chai'),
-    should = chai.should(),
-    path = require('path'),
-    fs = require('fs'),
-    suffixExtractor = require('../lib/index/suffix-extractor');
+  should = chai.should(),
+  path = require('path'),
+  fs = require('fs'),
+  suffixExtractor = require('../lib/index/suffix-extractor');
 
 describe('test UI hierarchical index h1 extractor', function () {
-    this.timeout(5000);
-
-    it('should extract parent-child tags', function (done) {
-        var file = [path.resolve(__dirname, '../fixtures/examples/index/me.pou.app-188.xml')];
-        var expected = fs.readFileSync(__dirname + '/../fixtures/examples/index/me.pou.app-188-ui-suffix.txt', 'utf8');
-        var target = path.resolve(__dirname , '../indexes/ui/suffix/');
-        suffixExtractor(file, target).then(() => {
-            var targetFile = path.resolve(__dirname, '../indexes/ui/suffix/me.pou.app-188-ui-suffix.txt');
-            var exists = fs.existsSync(targetFile);
-            exists.should.equal(true);
-            var result = fs.readFileSync(targetFile, 'utf8');
-            result.should.equal(expected);
-            done();
-        }).catch((e) => {
-            should.not.exist(e);
-            done(e);
-        });
+  this.timeout(20000);
+  it('should extract parent-child tags', (done) => {
+    const file = [path.resolve(__dirname, '../fixtures/examples/index/me.pou.app-188.xml')],
+      expected = fs.readFileSync(path.resolve(__dirname,
+           '/../fixtures/examples/index/me.pou.app-188-ui-suffix.txt'), 'utf8'),
+      target = path.resolve(__dirname, '../indexes/ui/suffix/');
+    suffixExtractor(file, target).then(() => {
+      const targetFile = path.resolve(__dirname,
+                           '../indexes/ui/suffix/me.pou.app-188-ui-suffix.txt'),
+        result = fs.readFileSync(targetFile, 'utf8');
+      result.should.equal(expected);
+      done();
     })
+    .catch((e) => {
+      should.not.exist(e);
+      done(e);
+    });
+  });
 });
--- test/ui-tag-extractor.test.js
+++ test/ui-tag-extractor.test.js
@@ -1,41 +1,46 @@
+/* eslint-env node, mocha */
+/* eslint no-console: 0 */
 'use strict';
 const chai = require('chai'),
-    should = chai.should(),
-    path = require('path'),
-    fs = require('fs'),
-    tagNameExtractor = require('../lib/index/tag-name-extractor');
+  should = chai.should(),
+  path = require('path'),
+  fs = require('fs'),
+  tagNameExtractor = require('../lib/index/tag-name-extractor');
 
-describe('test UI tag names and attributes extractor', function() {
-    this.timeout(5000);
-
-    it('should extract tag names and attributes', (done) => {
-        const file = [path.resolve(__dirname + '/../fixtures/examples/index/me.pou.app-188.xml')],
-            expected = fs.readFileSync(__dirname + '/../fixtures/examples/index/me.pou.app-188-ui-tag.txt', 'utf8'),
-            target = path.resolve(__dirname + '/../indexes/ui/tag');
-        tagNameExtractor(file, target, '-ui-tag').then(() => {
-            const targetFile = path.resolve(__dirname + '/../indexes/ui/tag/me.pou.app-188-ui-tag.txt'),
-                exists = fs.existsSync(targetFile);
-            exists.should.equal(true);
-            const result = fs.readFileSync(targetFile, 'utf8');
-            result.should.equal(expected);
-            done();
-        }).catch((e) => {
-            should.not.exist(e);
-            done(e);
-        });
+describe('test UI tag names and attributes extractor', function () {
+  this.timeout(5000);
+  it('should extract tag names and attributes', (done) => {
+    const file = [path.resolve(__dirname, '..', 'fixtures',
+                              'examples', 'index', 'me.pou.app-188.xml')],
+      expected = fs.readFileSync(path.resolve(__dirname, '..', 'fixtures', 'examples',
+                              'index', 'me.pou.app-188-ui-tag.txt'), 'utf8'),
+      target = path.resolve(__dirname, '..', 'indexes', 'ui', 'tag');
+    tagNameExtractor(file, target, '-ui-tag').then(() => {
+      const targetFile = path.resolve(__dirname, '', '..', 'indexes', 'ui',
+                                      'tag', 'me.pou.app-188-ui-tag.txt'),
+        result = fs.readFileSync(targetFile, 'utf8');
+      result.should.equal(expected);
+      done();
+    })
+    .catch((e) => {
+      should.not.exist(e);
+      done(e);
     });
+  });
 
-    it('should fail to extract tag names and attributes for invalid files', (done) => {
-        const file = [path.resolve(__dirname, '../fixtures/examples/index/no.such.file-123.xml')],
-            target = path.resolve(__dirname, '../no-such-dir');
-        tagNameExtractor(file, target, '-ui-tag').then(() => {
-            const targetFile = path.resolve(__dirname, '../indexes/ui/tag/no.such.file-123-ui-tag.txt'),
-                exists = fs.existsSync(targetFile);
-            exists.should.equal(false);
-            done();
-        }).catch((e) => {
-            should.not.exist(e);
-            done(e);
-        });
+  it('should fail to extract tag names and attributes for invalid files', (done) => {
+    const file = [path.resolve(__dirname, '..', 'fixtures', 'examples',
+                              'index', 'no.such.file-123.xml')],
+      target = path.resolve(__dirname, '..', 'no-such-dir');
+    tagNameExtractor(file, target, '-ui-tag').then(() => {
+      const targetFile = path.join(target, 'no.such.file-123-ui-tag.txt'),
+        exists = fs.existsSync(targetFile);
+      exists.should.equal(false);
+      done();
+    })
+    .catch((e) => {
+      should.not.exist(e);
+      done(e);
     });
+  });
 });
